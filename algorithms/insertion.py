2aa1f475c54df8b4a79e62824d9a97912991f946
WT000 <55751558+WT000@users.noreply.github.com>
2020-11-18 12:49:04 +0000

    Coded an insertion sort algorithm

---
+++ algorithms/insertion.py
@@ -0,0 +1,31 @@
+# Insertion sort function takes in the array
+def insertionsort(arrayinput):
+    # Loop through the array starting from the first index
+    # going to the final index
+    for divider in range(1, len(arrayinput)):
+        
+        # If the index is lower than the previous index, it means
+        # the previous index isn't sorted, we need to sort it!
+        if arrayinput[divider] < arrayinput[divider-1]:
+          
+          # Counter for the while loop
+          counter = divider
+          
+          # Until the counter reaches the end of the array and
+          # the number at the counter has a value lower than the data
+          # to the left, 'shift' (swap) the items in the array
+          # until it's in a sorted position.
+          # The sorted pos is reached when the index at counter
+          # has no greater values to the left of it
+          while (counter > 0 and arrayinput[counter] < arrayinput[counter-1]):
+            temp = arrayinput[counter-1]
+            arrayinput[counter-1] = arrayinput[counter]
+            arrayinput[counter] = temp
+            counter -= 1
+
+    # Once all data in the array has been sorted, return the
+    # array
+    return arrayinput
+
+array_to_sort = [17, 5, 64, 1, 42]
+print(insertionsort(array_to_sort))
